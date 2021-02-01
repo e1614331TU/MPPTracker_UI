130669ca5485e33401d69f661db911f3571a12c0
robin <robin@10.0.0.15>
2021-02-01 20:51:35 +0100

    update point_i

--- MPP_UI/src/base/MPP_Osci.java
+++ MPP_UI/src/base/MPP_Osci.java
@@ -48,6 +48,7 @@ public class MPP_Osci extends JPanel implements ActionListener, I_PCDI_Listener 
 	private int tableIdP = 2;
 	private int valNr_pointU = 14;
 	private int valNr_pointI = 15;
+	private int index_equalZeroU = 0;
 	
 	private Timer cyclicTimerSlow  = null;
 	private Timer cyclicTimerFast  = null;
@@ -227,8 +228,8 @@ public class MPP_Osci extends JPanel implements ActionListener, I_PCDI_Listener 
 	public void notifyParameterRead(PCDI_Parameter<?> parameter, int deviceId) {
 		// TODO Auto-generated method stub
 		if(parameter.getValueNumber()==this.valNr_pointI) {
-			this.data_U_point.clear();
-			this.data_U_point.add(Double.valueOf(parameter.getValue().toString()));
+			this.data_I_point.clear();
+			this.data_I_point.add(Double.valueOf(parameter.getValue().toString()));
 			this.chart.updateXYSeries(this.nameScope3, data_U_point, data_I_point, null);
 		}else if(parameter.getValueNumber()==this.valNr_pointU) {
 			this.data_U_point.clear();
@@ -302,6 +303,7 @@ public class MPP_Osci extends JPanel implements ActionListener, I_PCDI_Listener 
 	public void notifyTableRead(PCDI_TableData tableData, int deviceId) {
 		// TODO Auto-generated method stub
 		int minDataPoints=tableData.getIndexNr();
+		int minUequalzero =0;
 		if(tableData.getTableId()==this.tableIdI) {
 			this.data_I.add(tableData.getIndexNr(),(double)tableData.getValue());
 		}
@@ -311,14 +313,10 @@ public class MPP_Osci extends JPanel implements ActionListener, I_PCDI_Listener 
 		if(tableData.getTableId()==this.tableIdP) {
 			if(this.data_U.size()<minDataPoints)
 				minDataPoints=this.data_U.size();
-
 			this.data_P.add(tableData.getIndexNr(),(double)tableData.getValue());
-			this.chart.updateXYSeries(this.nameScope2, this.data_U.subList(0, minDataPoints), this.data_P.subList(0, minDataPoints), null);
+			this.chart.updateXYSeries(this.nameScope2, this.data_U.subList(minUequalzero, minDataPoints), this.data_P.subList(minUequalzero, minDataPoints), null);
 		}
-		if(this.data_P.size() == this.data_U.size() && this.data_U.size() == this.data_I.size()) {
-			this.chart.updateXYSeries(this.nameScope1, data_U, data_I, null);
-			
-		}
+
 		
 		
 	}
