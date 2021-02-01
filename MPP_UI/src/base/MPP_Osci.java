845da04df5f95b3151fbad0c29bbdf3981b9f803
robin <robin@10.0.0.15>
2021-02-01 21:42:27 +0100

    fix

--- MPP_UI/src/base/MPP_Osci.java
+++ MPP_UI/src/base/MPP_Osci.java
@@ -326,8 +326,8 @@ public class MPP_Osci extends JPanel implements ActionListener, I_PCDI_Listener 
 		// TODO Auto-generated method stub
 		int minDataPoints=tableData.getIndexNr();
 		int minUequalzero =0;
-		for(int i=1;i<minDataPoints;i++) {
-			if(this.data_U.get(i)==0)
+		for(int i=0;i<minDataPoints;i++) {
+			if(this.data_U.size()>=i && this.data_U.get(i)==0)
 				minUequalzero=i;
 		}
 		if(tableData.getTableId()==this.tableIdI) {
